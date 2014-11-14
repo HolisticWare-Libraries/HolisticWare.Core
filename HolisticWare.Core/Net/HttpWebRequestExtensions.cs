807a9dec519ad0fe4bd9f085d0b3cdf1a8116492
moljac <mcvjetko@holisticware.net>
2014-11-14 11:22:34 +0100

    Initial commit    HTTP extensions    Xamarin Forms samples

---
+++ HolisticWare.Core/Net/HttpWebRequestExtensions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Net
+{
+	/// <summary>
+	/// Install-Package Microsoft.Net.Http 
+	/// </summary>
+	public static partial class HttpWebRequestExtensions
+	{
+		public static Task<System.Net.HttpWebResponse> GetResponseAsync(this System.Net.HttpWebRequest request)
+		{
+			var taskComplete = new TaskCompletionSource<System.Net.HttpWebResponse>();
+
+			request.BeginGetResponse(asyncResponse =>
+			{
+				try
+				{
+					System.Net.HttpWebRequest responseRequest = (System.Net.HttpWebRequest)asyncResponse.AsyncState;
+					System.Net.HttpWebResponse someResponse = (System.Net.HttpWebResponse)responseRequest.EndGetResponse(asyncResponse);
+					taskComplete.TrySetResult(someResponse);
+				}
+				catch (System.Net.WebException webExc)
+				{
+					System.Net.HttpWebResponse failedResponse = (System.Net.HttpWebResponse)webExc.Response;
+					taskComplete.TrySetResult(failedResponse);
+				}
+			}, request);
+
+			return taskComplete.Task;
+		}
+
+
+		public async static Task<string> GetStringFromRequest(this System.Net.HttpWebRequest request, string postData = null)
+		{
+			if (!string.IsNullOrEmpty(postData))
+			{
+				byte[] requestBytes = Encoding.UTF8.GetBytes(postData);
+
+				request.Method = "POST";
+				request.ContentType = "application/x-www-form-urlencoded";
+				//request.ContentLength = requestBytes.Length;	// PCL nogo
+
+				using (var postStream = await request.GetRequestStreamAsync())
+				{
+					await postStream.WriteAsync(requestBytes, 0, requestBytes.Length);
+				}
+			}
+			else
+			{
+				request.Method = "GET";
+			}
+
+			var response = await request.GetResponseAsync();
+
+			string returnValue = null;
+
+			if (response != null)
+			{
+				using (var receiveStream = response.GetResponseStream())
+				{
+					using (var reader = new StreamReader(receiveStream))
+					{
+						returnValue = await reader.ReadToEndAsync();
+					}
+				}
+			}
+
+			return returnValue;
+		}
+
+	}
+}
---
+++ HolisticWare.Core/Net/WebRequestExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Net
+{
+	/// <summary>
+	/// Install-Package Microsoft.Net.Http 
+	/// </summary>
+	static public partial class WebRequestExtensions
+	{
+		private static Task<Stream> GetRequestStreamAsync(this System.Net.WebRequest request)
+		{
+			return Task.Factory.FromAsync<Stream>
+						(
+						  request.BeginGetRequestStream
+						, request.EndGetRequestStream
+						, null
+						);
+		}
+
+	}
+}
