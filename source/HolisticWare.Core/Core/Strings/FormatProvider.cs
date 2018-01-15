8566425cff1571e06bbf56eaac38ccebef139a23
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-01-15 18:45:05 +0100

    FormatProvider update

--- source/HolisticWare.Core/Core/Strings/FormatProvider.cs
+++ source/HolisticWare.Core/Core/Strings/FormatProvider.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace Core.Strings
@@ -40,17 +42,30 @@ namespace Core.Strings
                 ContentTextual = format
             };
 
+            System.Diagnostics.Debug.WriteLine($"Options = {options.ToString("singleline")}");
+
             StringBuilder sb = new StringBuilder();
 
             switch (format.ToUpperInvariant())
             {
                 case "CSHARP":
                 case "CS":
-                    Type t = o.GetType();
-                    string type_fq = t.FullName;
-                    string type = t.Name;
+                    TypeInfo ti = o.GetType().GetTypeInfo();
+                    string type_fq = ti.FullName;
+                    string type = ti.Name;
                     string variable_name = type.ToLowerInvariant();
-                    sb.AppendLine($"{type_fq} {variable_name}= new {type_fq} ();");
+                    if 
+                        (
+                            typeof(System.Collections.IEnumerable).GetTypeInfo().IsAssignableFrom(ti)
+                            ||
+                            ti.ImplementedInterfaces.Contains(typeof(System.Collections.IEnumerable))
+                        )
+                    {                        
+                    }
+                    else
+                    {
+                        sb.AppendLine($"{type_fq} {variable_name}= new {type_fq} ();");
+                    }
                     break;
                 default:
                     HandleOtherFormats(format, o);
@@ -68,11 +83,19 @@ namespace Core.Strings
         private string HandleOtherFormats(string format, object arg)
         {
             if (arg is IFormattable)
+            {
+                System.Diagnostics.Debug.WriteLine("HandleOtherFormats Object implements IFormattable");
                 return ((IFormattable)arg).ToString(format, CultureInfo.CurrentCulture);
+            }
             else if (arg != null)
+            {
+                System.Diagnostics.Debug.WriteLine("HandleOtherFormats Object does NOT implement IFormattable");
                 return arg.ToString();
+            }
             else
+            {
                 return String.Empty;
+            }
         }
 
         public object GetFormat(Type formatType)
