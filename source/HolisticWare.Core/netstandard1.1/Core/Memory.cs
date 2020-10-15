81c18d6ca6e58fd03ea5fe38d4e6a4acb957d91b
moljac <mcvjetko@holisticware.net>
2020-10-15 16:44:18 +0200

    source

---
+++ source/HolisticWare.Core/netstandard1.1/Core/Memory.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Core
+{
+	public static partial class Memory
+	{
+		public static string ToString<T>(this System.Memory<T> memory)
+		{
+            string result = null;
+
+            return result;
+
+		}
+
+		public static string ToStringCSharp<T>(this System.Memory<T> memory)
+		{
+            string result = null;
+
+            return result;
+
+		}
+	}
+}
---
+++ source/HolisticWare.Core/netstandard1.1/Core/Span.cs
@@ -0,0 +1,13 @@
+﻿namespace Core
+{
+	public static partial class Span
+	{
+		public static string ToString<T>(this System.Span<T> memory)
+		{
+            string result = null;
+
+            return result;
+
+		}
+	}
+}
